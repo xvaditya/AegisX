59ac60b9bf92806df079f4a923c818ad44f90c64
xvaditya <adityakadam20069@gmail.com>
2026-04-29 20:10:13 +0530

    Deploy frontend to GitHub Pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { FloatingAssistant } from './pages/FloatingAssistant';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/floating" element={<FloatingAssistant />} />
